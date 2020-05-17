cc28a0900810cf50c0699bc2a94df2a6dcc0d03c
Edwinsidharta <edwin0sidharta@gmail.com>
2020-05-18 05:03:38 +0700

    tambah ejs top up saldo dan hapus temp midtrans

--- midtrans/index.js
+++
@@ -1,6 +0,0 @@
-const Snap = require('./lib/snap');
-const CoreApi = require('./lib/coreApi');
-const MidtransError = require('./lib/midtransError');
-const Midtrans = { Snap, CoreApi, MidtransError }
-
-module.exports = Midtrans;
