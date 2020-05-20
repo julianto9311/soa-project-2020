ab5f1bb23e880eb8f491df5c4163e819d43bdb6f
Edwinsidharta <edwin0sidharta@gmail.com>
2020-05-20 16:31:50 +0700

    database ditambahi field gambar untuk multer

--- proyek_soa.sql
+++ proyek_soa.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 4.9.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 17, 2020 at 05:09 PM
--- Server version: 10.4.6-MariaDB
--- PHP Version: 7.3.9
+-- Waktu pembuatan: 20 Bulan Mei 2020 pada 11.30
+-- Versi server: 10.4.8-MariaDB
+-- Versi PHP: 7.3.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -27,7 +27,7 @@ USE `proyek_soa`;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `review`
+-- Struktur dari tabel `review`
 --
 
 DROP TABLE IF EXISTS `review`;
@@ -42,7 +42,7 @@ CREATE TABLE `review` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `review`
+-- Dumping data untuk tabel `review`
 --
 
 INSERT INTO `review` (`id_review`, `email`, `id_jenis`, `nama_jenis`, `jenis`, `comment`, `rating`) VALUES
@@ -52,7 +52,7 @@ INSERT INTO `review` (`id_review`, `email`, `id_jenis`, `nama_jenis`, `jenis`, `
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Struktur dari tabel `user`
 --
 
 DROP TABLE IF EXISTS `user`;
@@ -64,51 +64,54 @@ CREATE TABLE `user` (
   `password` varchar(255) NOT NULL,
   `tipe_user` int(11) NOT NULL,
   `saldo` int(11) NOT NULL,
-  `api_hit` int(11) NOT NULL
+  `api_hit` int(11) NOT NULL,
+  `gambar` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `user`
+-- Dumping data untuk tabel `user`
 --
 
-INSERT INTO `user` (`id_user`, `email`, `nama_lengkap`, `nomor_hp`, `password`, `tipe_user`, `saldo`, `api_hit`) VALUES
-(1, 'edwin@gmail.com', 'edwin sidharta', '08123123123', '123', 0, 988000, 24),
-(2, 'ferdinan@gmail.com', 'ferdinan gutana', '08123123123', '123', 0, 0, 7),
-(3, 'cosmas@gmail.com', 'cosmas YBG', '08123123123', '123', 0, 0, 10),
-(4, 'julianto@gmail.com', 'eliphaz julianto', '08123123123', '123', 1, 20000, 0),
-(5, 'bambang@gmail.com', 'bambang', '08123123123', '123', 0, 0, 10);
+INSERT INTO `user` (`id_user`, `email`, `nama_lengkap`, `nomor_hp`, `password`, `tipe_user`, `saldo`, `api_hit`, `gambar`) VALUES
+(1, 'edwin@gmail.com', 'edwin sidharta', '08123123123', '123', 0, 988500, 24, 'default.jpg'),
+(2, 'ferdinan@gmail.com', 'ferdinan gutana', '08123123123', '123', 0, 0, 7, 'default.jpg'),
+(3, 'cosmas@gmail.com', 'cosmas YBG', '08123123123', '123', 0, 0, 10, 'default.jpg'),
+(4, 'julianto@gmail.com', 'eliphaz julianto', '08123123123', '123', 1, 4923500, 0, 'default.jpg'),
+(5, 'bambang@gmail.com', 'bambang', '08123123123', '123', 0, 0, 10, 'default.jpg'),
+(6, 'bambang2@gmail.com', 'bambang', '08123123123', '123', 0, 0, 10, 'default.jpg'),
+(7, 'bambang23@gmail.com', 'bambang', '08123123123', '123', 0, 0, 10, 'default.jpg');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `review`
+-- Indeks untuk tabel `review`
 --
 ALTER TABLE `review`
   ADD PRIMARY KEY (`id_review`);
 
 --
--- Indexes for table `user`
+-- Indeks untuk tabel `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id_user`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `review`
+-- AUTO_INCREMENT untuk tabel `review`
 --
 ALTER TABLE `review`
   MODIFY `id_review` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `user`
+-- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
