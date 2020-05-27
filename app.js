390f435ad6ec8084b6a592349c444a04800870c1
billiartag <billy.gondokusuma@hotmail.com>
2020-05-27 14:14:03 +0700

    ganti port dan start script

--- app.js
+++ app.js
@@ -11,6 +11,6 @@ app.use("/217116589/",require("./routes/217116589"));
 app.use("/217116590/",require("./routes/217116590"));
 app.use("/217116595/",require("./routes/217116595"));
 
-app.listen(3000, function(){
-    console.log("Listening to port 3000");
+app.listen(80, function(){
+    console.log("Listening to port 80");
 });
